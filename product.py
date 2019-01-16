22f50967f26b06f76ebd80a30814f93f87800ae1
shang <tkaging@gmail.com>
2019-01-16 18:54:01 +0800

    addition

--- product.py
+++ product.py
@@ -4,7 +4,13 @@ while True:
 	if name == 'q':
 		break
 	price = input('請輸入商品價格： ')
+	price = int(price)
 	product.append([name, price])
 print(product)
 for p in product:
 	print(p[0], '的價格是： ', p[1])
+
+with open ('product.csv', 'w' , encoding = 'utf-8') as f:
+	f.write('商品,價格\n')
+	for p in product:
+		f.write(p[0] + ',' + str(p[1]) + '\n')
